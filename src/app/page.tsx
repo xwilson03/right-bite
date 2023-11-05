81878873dffae12be988568e7e320177cfb513c1
xwilson03 <xanderwilson2014@gmail.com>
2023-11-04 21:57:31 -0500

    adds initial scan and results pages
    Co-authored-by: Joshua Silva <joshuasilva414@users.noreply.github.com>
    Co-authored-by: Lasya Yakkala <siaxvii@users.noreply.github.com>
    Co-authored-by: Liam Murray <Lermatroid@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,50 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Yippee :D
-    </main>
-  )
+"use client";
+
+import { useZxing } from "react-zxing";
+import { useState } from 'react';
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Component() {
+
+    const [upc, setUpc] = useState("");
+    const router = useRouter();
+
+    const { ref } = useZxing({
+        onDecodeResult(result) {
+            router.push(`/results/${result.getText()}`);
+        },
+        timeBetweenDecodingAttempts: 150
+    });
+
+    return (
+        <div className="w-full min-h-screen bg-zinc-900">
+            <div className="static flex justify-left align-middle m-0 p-0 gap-x-2 bg-zinc-800">
+              <Image className="m-2" src="/right-bite-logo.png" alt="RightBite Logo" width={75} height={75} priority/>
+              <h1 className="my-6 text-4xl text-white font-bold">RightBite</h1>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+                <video ref={ref} />
+            </div>
+
+            <p className="flex justify-center text-lg text-zinc-400 mb-4">
+                Scan the barcode of a food item or input the barcode number to get a safety rating and information about its
+                ingredients.
+            </p>
+
+            <div className="flex flex-col items-center justify-between gap-3 mb-6"> 
+                <input
+                    aria-label="Enter barcode number"
+                    className="border border-zinc-700 px-2 py-1 rounded w-3/4 focus:outline-none focus:ring-2 focus:ring-primary-600 text-white bg-zinc-900"
+                    placeholder="Enter barcode number"
+                    type="text"
+                    value={upc}
+                    onChange={(e) => setUpc(e.target.value)}
+                />
+                <Link className="bg-white text-black px-3 py-2 rounded" href={`/results/${upc}`}>Scan</Link>
+            </div>
+        </div>
+    )
 }
---
+++ src/app/results/[upc]/page.tsx
@@ -0,0 +1,74 @@
+import * as cheerio from "cheerio";
+
+async function getData(upc: string) {
+    const res:string = await ((await fetch(`https://upcfoodsearch.com/search?s=${upc}`)).text());
+    const $ = cheerio.load(res);
+
+    const ingredients:string[] = [];
+    const links:string[] = [];
+    const ingredientLinks = $("#ingredientsStyled > p").find('a');
+    
+    ingredientLinks.each((i, el) => {
+        ingredients.push($(el).text());
+        if($(el).attr('href')){
+            links.push($(el).attr('href') as string);
+        }
+    });
+
+    console.log(ingredientLinks);
+    
+    ingredients.forEach(element => {
+        console.log(element);
+    });
+    
+    return {
+        data: {
+            links,
+            ingredients,
+        }
+    }
+}
+
+export default async function Page({params: {upc}}: {params: {upc: string}}) {
+    const {data: {links, ingredients}} = await getData(upc);
+  
+    return (
+        <div className="p-4 grid gap-6">
+            <div className="bg-green-900 p-4 rounded-lg">
+                <h2 className="font-semibold text-lg text-white">Safety Rating:</h2>
+                <div className="flex justify-between mt-2">
+                    <span className="text-red-500">Unsafe</span>
+                    <span className="text-yellow-500">Moderate</span>
+                    <span className="text-green-500">Safe</span>
+                </div>
+                <div className="relative pt-1">
+                    <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-green-200">
+                        <div
+                            className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
+                            style={{
+                                width: "70%",
+                            }}
+                        />
+                    </div>
+                </div>
+                <p className="text-sm text-zinc-400 mt-2">This product is generally safe based on its ingredients.</p>
+            </div>
+            <div>
+                <h2 className="font-semibold text-lg mb-2 text-white">Ingredients</h2>
+                <ul className="space-y-1">
+                    {ingredients.map((ingredient, i) => (
+                        <li key={i} className="text-sm text-zinc-400">{ingredient}</li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h2 className="font-semibold text-lg mb-2 text-white">Product Information</h2>
+                <p className="text-sm text-zinc-400">
+                    This is a brief description of the product. It&apos;s made by a company that has had no recalls or lawsuits.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export const revalidate = 60;
