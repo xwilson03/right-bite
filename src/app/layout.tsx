6f415d6f6ec424f21bcd37c2fdfaab6341a9c425
joshuasilva414 <joshuasilva414@gmail.com>
2023-11-05 13:25:01 -0600

    Logo alignment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,17 +21,20 @@ export default function RootLayout({
       <body className={inter.className + "h-full"}>
         <div className="w-full min-h-screen bg-zinc-900">
           <div className="flex m-0 p-0 gap-x-0.5 md:gap-x-2 bg-zinc-800">
-            <Link href="/"><Image
-              className="flex justify-left m-2 md:m-4 w-8 md:w-16 h-auto"
-              src="/right-bite-logo.png"
-              alt="RightBite Logo"
-              width={75}
-              height={75}
-              priority
-            />
+            <Link href="/" className="flex">
+              <Image
+                className="flex place-self-center justiy-left m-2 md:m-4 w-8 md:w-16 h-auto"
+                src="/right-bite-logo.png"
+                alt="RightBite Logo"
+                width={75}
+                height={75}
+                priority
+              />
             </Link>
-            
-            <h1 className="flex justify-center my-6 text-xl md:text-4xl text-white h-fit font-bold">RightBite</h1>
+
+            <h1 className="flex justify-center my-6 text-xl md:text-4xl text-white h-fit font-bold">
+              RightBite
+            </h1>
           </div>
           {children}
         </div>
